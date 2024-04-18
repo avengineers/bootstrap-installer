8ae03cdf94bb22284ec275bee0984ad45fcfd325
Guenther, Karsten (RDD-RM) <Karsten.Guenther@marquardt.com>
2024-04-18 11:47:49 +0200

    v1.6.2

--- install.ps1
+++ install.ps1
@@ -88,6 +88,6 @@ $InformationPreference = "Continue"
 $ErrorActionPreference = "Stop"
 
 # Clone the bootstrap repository (using a release tag)
-CloneOrPullGitTag -RepoUrl "https://github.com/avengineers/bootstrap.git" -Tag "v1.6.1" -TargetDirectory ".bootstrap"
+CloneOrPullGitTag -RepoUrl "https://github.com/avengineers/bootstrap.git" -Tag "v1.6.2" -TargetDirectory ".bootstrap"
 
 ## end of script
