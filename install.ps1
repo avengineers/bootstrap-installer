3d545bc56307d212090b4602d6b0c7d66d0b199f
cuinixam <22344617+cuinixam@users.noreply.github.com>
2026-01-08 16:46:58 +0100

    v1.19.0-rc1

--- install.ps1
+++ install.ps1
@@ -92,6 +92,6 @@ $InformationPreference = "Continue"
 $ErrorActionPreference = "Stop"
 
 # Clone the bootstrap repository (using a release tag)
-CloneOrPullGitTag -RepoUrl "https://github.com/avengineers/bootstrap.git" -Tag "v1.18.0" -TargetDirectory ".bootstrap"
+CloneOrPullGitTag -RepoUrl "https://github.com/avengineers/bootstrap.git" -Tag "v1.19.0-rc1" -TargetDirectory ".bootstrap"
 
 ## end of script
