5465d1737e4c3d49d2d408c65355b34279a0cfa4
Mollenkopf, Fabian (RDSS-RI) <Fabian.Mollenkopf@marquardt.com>
2026-05-07 14:08:44 +0200

    v1.20.0

--- install.ps1
+++ install.ps1
@@ -92,6 +92,6 @@ $InformationPreference = "Continue"
 $ErrorActionPreference = "Stop"
 
 # Clone the bootstrap repository (using a release tag)
-CloneOrPullGitTag -RepoUrl "https://github.com/avengineers/bootstrap.git" -Tag "v1.19.1" -TargetDirectory ".bootstrap"
+CloneOrPullGitTag -RepoUrl "https://github.com/avengineers/bootstrap.git" -Tag "v1.20.0" -TargetDirectory ".bootstrap"
 
 ## end of script
